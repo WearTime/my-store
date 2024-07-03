eca7ce070e1b88a7d6696d5bc374e1620a84b26e
WearTime <rizqiwiransyah0410@gmail.com>
2024-07-03 23:16:09 +0700

    feat: setup admin layout and middleware withAuth

---
+++ src/components/layouts/AdminLayout/index.tsx
@@ -0,0 +1,29 @@
+import Sidebar from "@/components/fragments/Sidebar";
+import styles from "./AdminLayout.module.scss";
+
+type PropsTypes = {
+  children: React.ReactNode;
+};
+
+const listSidebarItem = [
+  {
+    title: "Dashboard",
+    url: "/admin",
+    icon: "bxs-dashboard",
+  },
+  {
+    title: "Products",
+    url: "/admin/products",
+    icon: "bxs-box",
+  },
+];
+const AdminLayout = ({ children }: PropsTypes) => {
+  return (
+    <div className={styles.admin}>
+      <Sidebar lists={listSidebarItem} />
+      {children}
+    </div>
+  );
+};
+
+export default AdminLayout;
